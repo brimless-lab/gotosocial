1a7a14e9d9f163c7dba772ff33c579cb13fe0f24
jiedo <jiedohh@gmail.com>
2023-01-28 15:50:37 +0800

    bessage brand in email

--- internal/email/reset.go
+++ internal/email/reset.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	resetTemplate = "email_reset_text.tmpl"
-	resetSubject  = "GoToSocial Password Reset"
+	resetSubject  = "Bessage Password Reset"
 )
 
 func (s *sender) SendResetEmail(toAddress string, data ResetData) error {
